31dc1f31dd00415015baf542cc6db89a07a2c67b
Anggi Irawan <60735994+anggiirawan13@users.noreply.github.com>
2023-07-28 09:14:17 +0700

    Add files via upload

--- ip checker.py
+++ ip checker.py
@@ -1,7 +1,19 @@
 import socket
+import requests
+import pprint
+import json
 
 hostname = input('Enter a domain name: ')
 ipAddress = socket.gethostbyname(hostname)
 
+reqURL ='https://geolocation-db.com/jsonp/' + ipAddress
+resp = requests.get(reqURL)
+geolocation = resp.content.decode()
+geolocation = geolocation.split('(')[1].strip(')')
+geolocation = json.loads(geolocation)
+
 print(f'Domain Name: {hostname}')
-print(f'Ip Address: {ipAddress}')
+print(f'IP Address: {ipAddress}')
+
+for k, v in geolocation.items():
+    pprint.pprint(str(k) + ' : ' + str(v))
